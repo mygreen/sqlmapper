48263e717467102130958ad8e8b17bdafe486ba9
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2021-05-04 12:37:19 +0900

    JavaDocの追加、修正

--- sqlmapper-apt/src/main/java/com/github/mygreen/sqlmapper/apt/package-info.java
+++ sqlmapper-apt/src/main/java/com/github/mygreen/sqlmapper/apt/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * APTによるエンティティ情報をもとに処理を行う機能を提供します。
+ */
 package com.github.mygreen.sqlmapper.apt;
--- sqlmapper-metamodel/src/main/java/com/github/mygreen/sqlmapper/metamodel/package-info.java
+++ sqlmapper-metamodel/src/main/java/com/github/mygreen/sqlmapper/metamodel/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * メタモデルによる条件式の組み立て機能を提供します。
+ */
 package com.github.mygreen.sqlmapper.metamodel;
