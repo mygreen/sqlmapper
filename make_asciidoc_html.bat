92b4e82ae5240b61172b5aa238f147fa4c110e68
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2021-07-04 19:20:34 +0900

    マニュアルのパスを変更

--- make_asciidoc_html.bat
+++ make_asciidoc_html.bat
@@ -7,16 +7,16 @@ echo "Building AsciiDoc documentation"
 %~d0
 cd %~p0
 
-pushd .\src\site\asciidoc
+pushd .\docs
 
 rem rmdir /q /s _build
 call asciidoctor -D _build index.adoc
 
 popd
 
-rmdir /q /s .\target\site\asciidoc
-mkdir .\target\site\asciidoc
-xcopy /y /e .\src\site\asciidoc\_build .\target\site\asciidoc
+rmdir /q /s .\target\site\docs
+mkdir .\target\site\docs
+xcopy /y /e .\docs\_build .\target\site\docs
 
 REM github-pages‚Ìsphinx‘Î‰
 echo "" > .\target\site\.nojekyll
